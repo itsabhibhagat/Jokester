83ce334ffed9defd31fe7cdf3017766f99cf37c2
itsabhibhagat <bhilai.abhijeet@gmail.com>
2026-04-27 12:07:06 +0530

    Config changes

--- src/main/java/com/application/jokester/JokesterApplication.java
+++ src/main/java/com/application/jokester/JokesterApplication.java
@@ -4,10 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
-// EnableSpringDataWebSupport fixes the PageImpl serialization warning.
-// When searchJokes returns a Page, Spring now serializes it as a stable
-// PagedModel JSON structure instead of the raw PageImpl which has no
-// guaranteed JSON stability across Spring versions.
 @SpringBootApplication
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class JokesterApplication {
